a52d14973881fafa1001cfc8c3c3e4cab6e1ca32
pyrypajunen <pajunenpyry@gmail.com>
2021-11-02 12:51:21 +0200

    Open pdf file automatically

--- App-2-Flatmates-Bill/main.py
+++ App-2-Flatmates-Bill/main.py
@@ -1,5 +1,6 @@
 ###IMPORTS###
 from fpdf import FPDF
+import webbrowser
 
 class Bill:
     """
@@ -38,29 +39,38 @@ class PdfGenerator:
         pdf = FPDF(orientation="P", unit="pt", format="A4")
         pdf.add_page()
         
+        # Add Image
+        pdf.image("App-2-Flatmates-Bill\house.png", w=30, h=30)
+        
         # Formating PDF
         pdf.set_font(family="Times", size =24, style="B")
-        pdf.cell(w = 0, h = 80, txt = "Flatmates Bill", border=1, align= "C", ln = 1)
+        pdf.cell(w = 0, h = 80, txt = "Flatmates Bill", border=0, align= "C", ln = 1)
         
         # Insert period
-        pdf.cell(w=100,h=40, txt = "Period:", border=1)
-        pdf.cell(w=150,h=40, txt = bill.period, border=1, ln=1)
+        pdf.set_font(family="Times", size =20, style="B")
+        pdf.cell(w=100,h=40, txt = "Period:", border=0)
+        pdf.cell(w=150,h=40, txt = bill.period, border=0, ln=1)
         
-        # Insert name and due amount of th first flatmate
-        pdf.cell(w=100,h=40, txt = flatmate2.name, border=1)
-        pdf.cell(w=150,h=40, txt =  str(flatmate2.pays(bill, flatmate)), border=1, ln=1)
+        # Insert name and due amount of the first flatmate
+        pdf.set_font(family="Times", size =14)
+        pdf.cell(w=100,h=25, txt =  "".join(flatmate2.name), border=0)
+        pdf.cell(w=150,h=25, txt =  str(round(flatmate2.pays(bill, flatmate),2)), border=0, ln=1)
+        pdf.cell(w=100,h=25, txt =  "".join(flatmate.name), border=0)
+        pdf.cell(w=150,h=25, txt =  str(round(flatmate.pays(bill, flatmate2),2)), border=0, ln=1)
             
-        # naming a file and save it as a pdf
+        # Naming a file and save it as a pdf
         pdf.output(self.filename + "_" + bill.period + ".pdf")
+        
+        # Open PDF file automatically
+        webbrowser.open(self.filename  + "_" + bill.period + ".pdf")
 
-    
-    
+        
 the_bill = Bill(amount=50, period="January 2021")
 lennart = Flatmate(name ="Lennart", days_in_house = 30)
 adele = Flatmate(name ="Adele", days_in_house = 23)
 
-#print("lennart pays:",lennart.pays(bill = the_bill, flatmate2 = adele))
-#print("Adele pays:", adele.pays(bill = the_bill, flatmate2 = lennart))
+print("lennart pays:",lennart.pays(bill = the_bill, flatmate2 = adele))
+print("Adele pays:", adele.pays(bill = the_bill, flatmate2 = lennart))
 
 
 pdf_generator = PdfGenerator(filename = "report") #Write only filename(Do not write filename.filetype)
---
+++ App-2-Flatmates-Bill/test.py
@@ -0,0 +1,5 @@
+nimi = ["Lumi", "Pyry"]
+
+
+for nimi_muuttujassa in range(5):
+    print(nimi)
